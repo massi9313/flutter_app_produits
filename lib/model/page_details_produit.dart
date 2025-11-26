891da3aa525c20c79f400a2c842390bd9de876e1
massinissa mameri <massilockwood93@gmail.com>
2025-11-26 10:58:19 -0500

    cresation de la page detailsproduit

---
+++ lib/model/page_details_produit.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_produit/model/produit.dart';
+
+
+class Detailproduit extends StatelessWidget {
+  final Produit produit;
+
+  const Detailproduit({super.key, required this.produit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(produit.nom)),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("nom : ${produit.nom}",
+            style: TextStyle(fontSize: 22,
+            fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
+            Text("description :",
+            style: TextStyle(fontSize: 18)),
+            SizedBox(height: 5),
+            Text(
+              produit.description,
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+        ),
+    );
+  }
+}
