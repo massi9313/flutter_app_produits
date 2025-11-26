fc880b3483966e63d3f3368e231770d9aca66905
massinissa mameri <massilockwood93@gmail.com>
2025-11-26 10:41:30 -0500

    ajout du modele produit

---
+++ lib/model/produit.dart
@@ -0,0 +1,11 @@
+class Produit {
+  final String nom;
+  final String description;
+  final double prix;
+
+  Produit({
+    required this.nom,
+    required this.description,
+    required this.prix,
+  });
+}
